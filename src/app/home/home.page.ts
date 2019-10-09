88e528ba9eff2e9fdd1a2ee73ccf48007968a8eb
rubenpachecomatas <ruben.pacheco.matas@gmail.com>
2019-10-09 22:16:45 +0200

    calculator-ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  result: string = '0';
+  number = 0;
+  numberAux = 0;
+  numberSelected;;
+  operator = '';
+
   constructor() {}
+
+  calculate() {
+    this.result = this.number + this.operator + this.numberAux;
+  }
+
+  clickNumber(numberSelected) {
+    if (this.number == 0) {
+      this.number = this.number * 10 + numberSelected;
+
+
+    } else if (this.numberAux == 0) {
+      this.numberAux = this.numberAux * 10 + numberSelected;
+    }
+  }
+
+  click
 
 }
